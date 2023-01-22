159defe337b94be377c7c464b2d1a0d9a1c45f65
Cj Carpon <114817092+cjimmortal@users.noreply.github.com>
2023-01-23 00:01:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,168 @@
+
+// this code is for category focus
+function  displayPlaylist2(){
+    document.getElementById("playlist-2").style.display="flex"; //enable the playlist 2
+    document.getElementById("c-focus").style.visibility="hidden";
+    document.getElementById("c-showall").style.visibility="hidden";
+
+    document.getElementById("c-2-spotify").style.visibility="hidden";
+    document.getElementById("c-2-showall").style.visibility="hidden";
+
+    document.getElementById("playlist-2nd").style.display="none";
+    document.getElementById("playlist-2nd-2").style.display="none";
+
+    document.getElementById("left").style.pointerEvents="block";
+    document.getElementById("left").style.opacity="1";
+}
+
+
+        function hoverPlaylist1(x) {
+            document.getElementById("p-f-play1").style.display="block";
+        }
+        function NoHoverPlaylist1(x) {
+            document.getElementById("p-f-play1").style.display="none";
+        }
+
+        function hoverPlaylist2(x) {
+            document.getElementById("p-f-play2").style.display="block";
+        }
+        function NoHoverPlaylist2(x) {
+            document.getElementById("p-f-play2").style.display="none";
+        }
+
+        function hoverPlaylist3(x) {
+            document.getElementById("p-f-play3").style.display="block";
+        }
+        function NoHoverPlaylist3(x) {
+            document.getElementById("p-f-play3").style.display="none";
+        }
+
+        function hoverPlaylist4(x) {
+            document.getElementById("p-f-play4").style.display="block";
+        }
+        function NoHoverPlaylist4(x) {
+            document.getElementById("p-f-play4").style.display="none";
+        }
+
+        function hoverPlaylist5(x) {
+            document.getElementById("p-f-play5").style.display="block";
+        }
+        function NoHoverPlaylist5(x) {
+            document.getElementById("p-f-play5").style.display="none";
+        }
+
+        function hoverPlaylist6(x) {
+            document.getElementById("p-f-play6").style.display="block";
+        }
+        function NoHoverPlaylist6(x) {
+            document.getElementById("p-f-play6").style.display="none";
+        }
+
+        function hoverPlaylist7(x) {
+            document.getElementById("p-f-play7").style.display="block";
+        }
+        function NoHoverPlaylist7(x) {
+            document.getElementById("p-f-play7").style.display="none";
+        }
+
+        function hoverPlaylist8(x) {
+            document.getElementById("p-f-play8").style.display="block";
+        }
+        function NoHoverPlaylist8(x) {
+            document.getElementById("p-f-play8").style.display="none";
+        }
+
+        function hoverPlaylist9(x) {
+            document.getElementById("p-f-play9").style.display="block";
+        }
+        function NoHoverPlaylist9(x) {
+            document.getElementById("p-f-play9").style.display="none";
+        }
+
+        function hoverPlaylist10(x) {
+            document.getElementById("p-f-play10").style.display="block";
+        }
+        function NoHoverPlaylist10(x) {
+            document.getElementById("p-f-play10").style.display="none";
+        }
+
+        // this code is for category focus
+
+
+        // this code is for category spotify playlist
+        function  display2ndPlaylist2(){
+            document.getElementById("playlist-2nd-2").style.display="flex"; //enable the playlist 2
+            document.getElementById("c-focus").style.display="none";
+            document.getElementById("c-showall").style.display="none";
+
+            document.getElementById("c-2-spotify").style.display="none";
+            document.getElementById("c-2-showall").style.display="none";
+
+            document.getElementById("playlist").style.display="none";
+            document.getElementById("playlist-2").style.display="none";
+
+            document.getElementById("left").style.pointerEvents="block";
+            document.getElementById("left").style.opacity=1;
+        
+        }
+
+
+        function hover2ndPlaylist1(x) {
+            document.getElementById("p-s-play1").style.display="block";
+        }
+        function NoHover2ndPlaylist1(x) {
+            document.getElementById("p-s-play1").style.display="none";
+        }
+
+        function hover2ndPlaylist2(x) {
+            document.getElementById("p-s-play2").style.display="block";
+        }
+        function NoHover2ndPlaylist2(x) {
+            document.getElementById("p-s-play2").style.display="none";
+        }
+
+        function hover2ndPlaylist3(x) {
+            document.getElementById("p-s-play3").style.display="block";
+        }
+        function NoHover2ndPlaylist3(x) {
+            document.getElementById("p-s-play3").style.display="none";
+        }
+
+        function hover2ndPlaylist4(x) {
+            document.getElementById("p-s-play4").style.display="block";
+        }
+        function NoHover2ndPlaylist4(x) {
+            document.getElementById("p-s-play4").style.display="none";
+        }
+
+        function hover2ndPlaylist5(x) {
+            document.getElementById("p-s-play5").style.display="block";
+        }
+        function NoHover2ndPlaylist5(x) {
+            document.getElementById("p-s-play5").style.display="none";
+        }
+
+        function hover2ndPlaylist6(x) {
+            document.getElementById("p-s-play6").style.display="block";
+        }
+        function NoHover2ndPlaylist6(x) {
+            document.getElementById("p-s-play6").style.display="none";
+        }
+
+        function hover2ndPlaylist7(x) {
+            document.getElementById("p-s-play7").style.display="block";
+        }
+        function NoHover2ndPlaylist7(x) {
+            document.getElementById("p-s-play7").style.display="none";
+        }
+
+        function hover2ndPlaylist8(x) {
+            document.getElementById("p-s-play8").style.display="block";
+        }
+        function NoHover2ndPlaylist8(x) {
+            document.getElementById("p-s-play8").style.display="none";
+        }
+
+       
+
+         // this code is for category spotify playlist
